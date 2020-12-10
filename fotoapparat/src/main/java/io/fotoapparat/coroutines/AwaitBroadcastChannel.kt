9ac66c5962df9890b41e6b4604ac494025a18d2a
André Abreu <andre.abreu@picpay.com>
2020-12-10 15:09:07 -0300

    bumping coroutines

--- fotoapparat/src/main/java/io/fotoapparat/coroutines/AwaitBroadcastChannel.kt
+++ fotoapparat/src/main/java/io/fotoapparat/coroutines/AwaitBroadcastChannel.kt
@@ -1,14 +1,17 @@
 package io.fotoapparat.coroutines
 
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.channels.ConflatedBroadcastChannel
 
 /**
  * A [ConflatedBroadcastChannel] which exposes a [getValue] which will [await] for at least one value.
  */
-internal class AwaitBroadcastChannel<T>(
+@OptIn(ExperimentalCoroutinesApi::class)
+internal class AwaitBroadcastChannel<T> constructor(
         private val channel: ConflatedBroadcastChannel<T> = ConflatedBroadcastChannel(),
         private val deferred: CompletableDeferred<Boolean> = CompletableDeferred()
 ) : BroadcastChannel<T> by channel, Deferred<Boolean> by deferred {
@@ -32,6 +35,12 @@ internal class AwaitBroadcastChannel<T>(
     }
 
     override fun cancel(cause: Throwable?): Boolean {
-        return channel.cancel(cause) && deferred.cancel(cause)
+        cancel(CancellationException(message = cause?.message, cause = cause))
+        return true
+    }
+
+    override fun cancel(cause: CancellationException?) {
+        channel.cancel(cause)
+        deferred.cancel(cause)
     }
 }
